c8b011cd15570e526d84dfe03070f0ca14da61be
Sam Meacham <diesam@gmail.com>
2013-10-30 15:41:39 -0700

    Added a script to help create noefgen-net2.exe

---
+++ scripts/create-merged-noefgen-net2.bat
@@ -0,0 +1,2 @@
+md ..\deploy\NoefGen_Merged
+..\tools\ilmerge\ilmerge.exe /targetplatform:v4,"C:\Windows\Microsoft.NET\Framework64\v4.0.30319" /ndebug /out:../deploy/NoefGen_Merged/NoefGen-net2.exe ../deploy/NoefGen/NoefGen.exe ../deploy/NoefGen/Ionic.Zip.dll ../deploy/NoefGen/Noef.CodeGen.dll

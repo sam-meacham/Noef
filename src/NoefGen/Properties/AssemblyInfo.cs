afb49df6c21387299884d08f0387fa75508e2e07
Sam Meacham <diesam@gmail.com>
2013-12-06 10:46:16 -0800

    Pushed NoefGen and Noef.SourceDistro to prod nuget to test in lamchops.

--- src/NoefGen/Properties/AssemblyInfo.cs
+++ src/NoefGen/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2013.1206.0058.0")]
-[assembly: AssemblyFileVersion("2013.1206.0058.0")]
+[assembly: AssemblyVersion("2013.1206.1043.0")]
+[assembly: AssemblyFileVersion("2013.1206.1043.0")]
